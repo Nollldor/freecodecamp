c7683b48142e74b41a2bf68e338a792bab1cee4d
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-05-04 12:32:28 +0300

    Build JavaScript Objects.js

---
+++ js/Basic JavaScript/Build JavaScript Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Gerda",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Kristine", "Ayrat", "Me"]
+
+    // Only change code above this line
+};
