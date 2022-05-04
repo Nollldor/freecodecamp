5d49a04f481bf8d0405be7b341c445dc84c15f77
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-05-04 13:14:33 +0300

    Manipulating Complex Objects.js

---
+++ js/Basic JavaScript/Manipulating Complex Objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+
+    {
+        "artist": "Arthur Nolldor",
+        "title": "bla-bla",
+        "release_year": 2022,
+        "formats": [
+            "CD",
+            "online",
+        ],
+        "gold": false
+    }
+];
